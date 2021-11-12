ca0447cd95b431c19d60f6203cf6423122611b3e
Ali Fayed <ali.fayed2666@gmail.com>
2021-11-12 17:56:03 +0200

    Initial Commit

---
+++ Advanced Networking Layer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Advanced Networking Layer
+//
+//  Created by Ali Fayed on 12/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Advanced Networking LayerUITests/Advanced_Networking_LayerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Advanced_Networking_LayerUITestsLaunchTests.swift
+//  Advanced Networking LayerUITests
+//
+//  Created by Ali Fayed on 12/11/2021.
+//
+
+import XCTest
+
+class Advanced_Networking_LayerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
